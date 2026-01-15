59332a14b0632efe373000067dcc2be7d9c0983b
Akhere Ihoeghinlan <101akhere5@gmail.com>
2026-01-15 20:13:45 +0000

    Added multiple error messages

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,35 +5,46 @@ export default function Contact(){
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
-    const [errorMessage, setErrorMessage] = useState("")
+    const [errorMessages, setErrorMessages] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
 
     function validate({ name, email, message }){
-        let errorMessage = ""
+        let errors = {
+            name: "",
+            email: "",
+            message: ""
+        }
+
         if (name.trim().length < 3){
-            errorMessage = "Name is too short"
+            errors.name = "Name is too short"
         }
         if (name.length > 30){
-            errorMessage = "Name is too long"
+            errors.name = "Name is too long"
         } 
         if (email.trim().length < 3){
-            errorMessage = "email is too short"
+            errors.email = "email is too short"
         }
         if (email.length > 30){
-            errorMessage = "email is too long"
+            errors.email = "email is too long"
         } 
         if (message.trim().length < 3){
-            errorMessage = "Message is too short"
+            errors.message = "Message is too short"
         }
         if (message.length > 30){
-            errorMessage = "Message is too long"
+            errors.message = "Message is too long"
         } 
-        setErrorMessage(errorMessage) 
+        setErrorMessages(errors) 
+
+        return Object.values(errors).join("") !== ""
 
     }
 
     function handleSubmit(e){
         e.preventDefault()
-        if (errorMessage !== ""){
+        if (validate({name, email, message})){
             return
         }
 
@@ -43,11 +54,9 @@ export default function Contact(){
             body: JSON.stringify({ name: name, email: email, message: message })
         })
 
-        setErrorMessage("")
         setName("")
         setEmail("")
         setMessage("")
-
         alert("Success")
     }
 
@@ -55,13 +64,15 @@ export default function Contact(){
     <div>
         <h1>Contact</h1>
         <form className="form" >
-            {errorMessage !== "" && <p>{errorMessage}</p>}
             <label>Name</label>
-            <input value={name} onChange={e => {setName(e.target.value); validate({name: e.target.value, email: email, message: message }) }} />
+            <input value={name} onChange={e => {setName(e.target.value); validate({name: e.target.value, email, message }) }} />
+            {errorMessages.name && <p className="errorMessage">{errorMessages.name}</p>}
             <label>Email</label>
-            <input value={email} onChange={e => {setEmail(e.target.value); validate({name: name, email: e.target.value, message: message })}} />
+            <input value={email} onChange={e => {setEmail(e.target.value); validate({name, email: e.target.value, message })}} />
+            {errorMessages.email && <p className="errorMessage">{errorMessages.email}</p>}
             <label>Message</label>
-            <textarea value={message} onChange={e => {setMessage(e.target.value); validate({name: name, email: email, message: e.target.value })}}/>
+            <textarea value={message} onChange={e => {setMessage(e.target.value); validate({name, email, message: e.target.value })}}/>
+            {errorMessages.message && <p className="errorMessage">{errorMessages.message}</p>}
             <button onClick={handleSubmit}>Send Query</button>
         </form>
     </div>
