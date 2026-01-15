1291ff7f576808d7d139179c7f1cf040bf658892
Akhere Ihoeghinlan <101akhere5@gmail.com>
2026-01-15 19:43:11 +0000

    Added simple contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react"
+import "./Contact.css"
+
 export default function Contact(){
-    return <div>
-        <p>Contact</p>
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
+    function validate(){
+        let errorMessage = ""
+        if (name.trim().length < 3){
+            errorMessage = "Name is too short"
+        }
+        if (name.length > 30){
+            errorMessage = "Name is too long"
+        } 
+        if (email.trim().length < 3){
+            errorMessage = "email is too short"
+        }
+        if (email.length > 30){
+            errorMessage = "email is too long"
+        } 
+        if (message.trim().length < 3){
+            errorMessage = "Message is too short"
+        }
+        if (message.length > 30){
+            errorMessage = "Message is too long"
+        } 
+        return errorMessage 
+
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const error = validate()
+
+        if (error){
+            setErrorMessage(error)
+            return
+        }
+
+
+        fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: name, email: email, message: message })
+        })
+
+        setErrorMessage("")
+        setName("")
+        setEmail("")
+        setMessage("")
+
+        alert("Success")
+    }
+
+    return (
+    <div>
+        <h1>Contact</h1>
+        <form className="form" >
+            {errorMessage !== "" && <p>{errorMessage}</p>}
+            <label>Name</label>
+            <input value={name} onChange={e => {setName(e.target.value);}} />
+            <label>Email</label>
+            <input value={email} onChange={e => {setEmail(e.target.value);}} />
+            <label>Message</label>
+            <textarea value={message} onChange={e => {setMessage(e.target.value);}}/>
+            <button onClick={handleSubmit}>Send Query</button>
+        </form>
     </div>
+    )
 }
